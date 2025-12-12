6d9dcfadece804417f5a552a3d4a2af08d8a141a
kosumoff <kosumoff@gmail.com>
2025-12-12 18:55:05 +0500

    Define domain entities

---
+++ internal/domain/assignment.go
@@ -0,0 +1,6 @@
+package domain
+
+type Assignment struct {
+	Giver    Participant
+	Receiver Participant
+}
---
+++ internal/domain/participant.go
@@ -0,0 +1,7 @@
+package domain
+
+type Participant struct {
+	ChatID   int64
+	UserID   int64
+	Username string
+}
